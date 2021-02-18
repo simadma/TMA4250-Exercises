14cd83d9d89171cf9324e9bb9c618c42fbc288b1
Simadma <madsadrian@gmail.com>
2021-02-18 21:03:09 +0100

    added d) and e)

--- R/ex01_problem02.R
+++ R/ex01_problem02.R
@@ -9,21 +9,17 @@ library(tidyverse)
 # Read data
 data_path <- "./Data/"
 fig_path <- "./Figures/"
-data <- read.table(
-          file = paste0(data_path, "topo.dat"),
-          header = TRUE
-        )
+data <- read.table(file = paste0(data_path, "topo.dat"), header = TRUE)
 
 n <- 315
-
-# Plot data
-res <- akima::interp(
-         x = data$x, y = data$y, z = data$z,
-         xo = seq(0, n, by = 5), yo = seq(0, n, by = 5),
-         linear = FALSE,
-         extrap = TRUE
-       )
-
+spl <- with(data,
+            interp(x, y, z,
+                   xo     = seq(0, n, by = 5),
+                   yo     = seq(0, n, by = 5),
+                   linear = FALSE,
+                   extrap = TRUE
+            )
+)
 
 # Plot
 # pdf(
@@ -31,44 +27,105 @@ res <- akima::interp(
 #   width = 6,
 #   height = 5.75
 # )
-
-fields::image.plot(res, col = terrain.colors(n = 256), asp = 1)
-contour(res, nlevels = 11, add = TRUE)
-points(x = data$x, y = data$y, pch = 4, col = "white")
-points(x = data$x, y = data$y, pch = 3, col = "black")
+image.plot(spl, col = terrain.colors(n = 256), asp = 1)
+contour(spl, nlevels = 11, add = TRUE)
+with(data, points(x, y, pch = 4, col = "white"))
+with(data, points(x, y, pch = 3, col = "black"))
 
 # dev.off()
 
 ## c)
-gridline <- seq(1, n, length.out = 50)
-locs <- expand.grid(gridline, gridline)
+m = 100
+gridline <- seq(1, n, length.out = m)
+locs <- expand.grid(x = gridline, y = gridline)
 geodata <- as.geodata(data)
-kc <- krige.conv(
-        geodata   = geodata,
-        locations = locs,
-        borders   = NULL,
-        krige     = krige.control(
-                      type.krige = "ok",      # Ordinary Kriging
-                      trend.d    = "cte",     # Constant mean of data values
-                      trend.l    = "cte",     # Constant mean at prediction locations
-                      cov.model  = "powered.exponential",
-                      cov.pars   = c(2500, 100),  # (sigma_r^2, xi)
-                      kappa      = 1.5            # nu
-                    )
-      )
-persp(kc, theta = 135, phi = 25)
-
-
-# Plot
-pdf(
-  file = paste0(fig_path, "kriging2.pdf")#,
-  #width = 6,
-  #height = 5.75
+ord_kc <- krige.conv(
+  geodata   = geodata,
+  locations = locs,
+  krige     = krige.control(
+    type.krige = "ok",       # Ordinary Kriging
+    trend.d    = "cte",      # Constant mean of data values
+    trend.l    = "cte",      # Constant mean at prediction locations
+    cov.model  = "powered.exponential",
+    cov.pars   = c(2500, 100),  # (sigma_r^2, xi)
+    kappa      = 1.5            # nu
+  )
 )
 
-image(kc, col = terrain.colors(n = 256))
-contour(kc, add = TRUE)
-contour(kc,filled=TRUE,coords.data=geodata$coords,color=terrain.colors)
+ok <- list(
+  x   = gridline,
+  y   = gridline,
+  z   = matrix(ord_kc$predict, nrow = m),
+  var = matrix(ord_kc$krige.var, nrow = m)
+)
+image.plot(ok, col = terrain.colors(n = 256), asp = 1)
+contour(ok, nlevels = 11, add = TRUE)
+with(data, points(x, y, pch = 4, col = "white"))
+with(data, points(x, y, pch = 3, col = "black"))
+
+with(ok, image.plot(x, y, var, col = inferno(n = 256), asp = 1))
+with(ok, contour(x, y, var, nlevels = 10, add = TRUE))
+with(data, points(x, y, pch = 4, col = "white"))
+with(data, points(x, y, pch = 3, col = "black"))
 
 
-dev.off()
+## d)
+uni_kc <- krige.conv(
+  geodata   = geodata,
+  locations = locs,
+  krige     = krige.control(
+    type.krige = "ok",  # Universal Kriging, specified with trend below
+    trend.d = trend.spatial(~ x + y + I(x * y) + I(x^2) + I(y^2), geodata),
+    trend.l = with(locs, trend.spatial(~ x + y + I(x * y) + I(x^2) + I(y^2))),
+    cov.model  = "powered.exponential",
+    cov.pars   = c(2500, 100),  # (sigma_r^2, xi)
+    kappa      = 1.5            # nu
+  )
+)
+
+uk <- list(
+  x   = gridline,
+  y   = gridline,
+  z   = matrix(uni_kc$predict, nrow = m),
+  var = matrix(uni_kc$krige.var, nrow = m)
+)
+
+image.plot(uk, col = terrain.colors(n = 256), asp = 1)
+contour(uk, nlevels = 11, add = TRUE)
+with(data, points(x, y, pch = 4, col = "white"))
+with(data, points(x, y, pch = 3, col = "black"))
+
+with(uk, image.plot(x, y, var, col = inferno(n = 256), asp = 1))
+with(uk, contour(x, y, var, nlevels = 10, add = TRUE))
+with(data, points(x, y, pch = 4, col = "white"))
+with(data, points(x, y, pch = 3, col = "black"))
+
+
+
+## e)
+ord_kc2 <- krige.conv(
+  geodata   = geodata,
+  locations = expand.grid(1:n, 1:n),
+  krige     = krige.control(
+    type.krige = "ok",       # Ordinary Kriging
+    trend.d    = "cte",      # Constant mean of data values
+    trend.l    = "cte",      # Constant mean at prediction locations
+    cov.model  = "powered.exponential",
+    cov.pars   = c(2500, 100),  # (sigma_r^2, xi)
+    kappa      = 1.5            # nu
+  )
+)
+
+ok2 <- list(
+  x   = gridline,
+  y   = gridline,
+  z   = matrix(ord_kc2$predict, nrow = n),
+  var = matrix(ord_kc2$krige.var, nrow = n)
+)
+x0 <- 100
+y0 <- 100
+r0_hat <- ok2$z[x0, y0]          # Kriging predictor at (x0, y0) = (100, 100)
+sigma_r_hat2 <- ok2$var[x0, y0]  # Prediction variance at (x0, y0) = (100, 100)
+
+pnorm(850, mean = r0_hat, sd = sqrt(sigma_r_hat2), lower.tail = FALSE)  # Pr(r0 > 850 | d)
+qnorm(0.9, mean = r0_hat, sd = sqrt(sigma_r_hat2))  # z such that Pr(r0 < z | d) = 0.90
