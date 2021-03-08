3313cb758c4522a4544cb9baaf9b9548c3a4c69e
Mads Adrian Simonsen <47949834+simadma@users.noreply.github.com>
2021-03-08 16:58:37 +0100

    changed axis limits in Figure Ex01a

--- Exercise 2/R/ex02_problem01.R
+++ Exercise 2/R/ex02_problem01.R
@@ -20,34 +20,42 @@ pines <- read_table(paste0(data_path, "pines.dat"), col_names = c("x", "y"), ski
 
 p1 <- ggplot(cells, aes(x, y)) +
   geom_point() +
-  coord_fixed() +
-  labs(title = "Biological Cells Point Pattern") +
+  coord_fixed(
+    xlim = c(0, 1),
+    ylim = c(0, 1)
+  ) +
   theme_bw()
 
 p2 <- ggplot(redwood, aes(x, y)) +
   geom_point() +
-  coord_fixed() +
-  labs(title = "California Redwoods Point Pattern") +
+  coord_fixed(
+    xlim = c(0, 1),
+    ylim = c(0, 1)
+  ) +
   theme_bw()
 
 p3 <- ggplot(pines, aes(x, y)) +
   geom_point() +
-  coord_fixed() +
-  labs(title = "Japanese Pines Point Pattern") +
+  coord_fixed(
+    xlim = c(0, 1),
+    ylim = c(0, 1)
+  ) +
   theme_bw()
 
 
 ## Save figures
 
+# w <- 7  # cm
+# h <- 7  # cm
 # ggsave("cells.pdf",
 #   plot = p1, path = fig_path,
-#   width = 9, height = 9, units = "cm"
+#   width = w, height = h, units = "cm"
 # )
 # ggsave("redwood.pdf",
 #        plot = p2, path = fig_path,
-#        width = 9, height = 9, units = "cm"
+#        width = w, height = h, units = "cm"
 # )
 # ggsave("pines.pdf",
 #        plot = p3, path = fig_path,
-#        width = 9, height = 9, units = "cm"
+#        width = w, height = h, units = "cm"
 # )
