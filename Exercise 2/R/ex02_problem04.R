34784bf32ba3f1f3b5a53a03e57007243173d205
Mads Adrian Simonsen <47949834+simadma@users.noreply.github.com>
2021-03-23 22:52:27 +0100

    finished 3

--- Exercise 2/R/ex02_problem04.R
+++ Exercise 2/R/ex02_problem04.R
@@ -1,34 +1,25 @@
-#Problem 2
+#Exercise 2
 #Authors: Mads Adrian Simonsen and Karina Lilleborge
 #Date: 03-23-21
 
 #libraries
-library(tidyverse)
-library(spatial)
+source("./Exercise 2/R/utils.R")
+##########################################################################################
+#                                     PROBLEM 4                                          #
+##########################################################################################
 
-#################################################
-#           PROBLEM 4                           #
-#################################################
-
-#paths
-data_path <- "./Exercise 2/Data/"
-fig_path <- "./Exercise 2/Figures/"
 
 ## Figure size
 w <- 10  # cm
 h <- 6  # cm
 
 
-#Get data
+## Read data
 # Biological Cells Point Pattern
 cells <- read_table(paste0(data_path, "cells.dat"), col_names = c("x", "y"))
 
 ##########################################################################################
 #                                 Subproblem a)                                          #
 ##########################################################################################
-
-
-
-
 
 
