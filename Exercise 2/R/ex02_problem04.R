061bff1857ba25263d1d038b66eae64004a27f9d
Mads Adrian Simonsen <47949834+simadma@users.noreply.github.com>
2021-03-24 00:03:39 +0100

    progress on 4

--- Exercise 2/R/ex02_problem04.R
+++ Exercise 2/R/ex02_problem04.R
@@ -9,9 +9,6 @@ source("./Exercise 2/R/utils.R")
 ##########################################################################################
 
 
-## Figure size
-w <- 10  # cm
-h <- 6  # cm
 
 
 ## Read data
@@ -22,4 +19,63 @@ cells <- read_table(paste0(data_path, "cells.dat"), col_names = c("x", "y"))
 #                                 Subproblem a)                                          #
 ##########################################################################################
 
+# Biological Cells point pattern
+cells %>% ggplot(aes(x, y)) +
+  geom_point() +
+  coord_fixed(
+    xlim = c(0, 1),
+    ylim = c(0, 1)
+  ) +
+  theme_bw()
 
+plot_Lf(cells)
+set.seed(10)
+g <- Strauss(42,c=0.04, r = 0.10)
+plot_Lf(g)
+data.frame(x=g$x, y=g$y) %>% 
+  ggplot(aes(x, y)) +
+  geom_point() +
+  coord_fixed(
+    xlim = c(0, 1),
+    ylim = c(0, 1)
+  ) +
+  theme_bw()
+
+
+phi <- function(tau, tau_0, phi_0, phi_1) {
+  tau <- norm(tau, type = "2")
+  if_else(tau <= tau_0,
+    true  = phi_0,
+    false = phi_0*exp(-phi_1*(tau - tau_0))
+  )
+}
+
+iterative_sim_Strauss <- function(k, tau_0, phi_0, phi_1, n_iter = 1000) {
+  X_D <- matrix(runif(2*k), nrow = k, dimnames = list(NULL, c("x", "y")))
+  for (i in 1:n_iter) {
+    u <- sample(k, 1) # sample index
+    x_p <- runif(2)   # proposal
+    exponent <- 0
+    for (j in 1:k) {
+      exponent <- exponent + (phi(x_p - X_D[j, ], tau_0, phi_0, phi_1)
+                              - phi(X_D[u, ] - X_D[j, ], tau_0, phi_0, phi_1))
+    }
+    alpha <- min(1, exp(-exponent))
+    if (runif(1) < alpha) {
+      X_D[u, ] <- x_p
+    }
+  }
+  as_tibble(X_D)
+}
+
+
+test <- iterative_sim_Strauss(nrow(cells), tau_0 = 0.08, phi_0 = 5, phi_1 = 6)
+
+test %>% 
+  ggplot(aes(x, y)) +
+  geom_point() +
+  coord_fixed(
+    xlim = c(0, 1),
+    ylim = c(0, 1)
+  ) +
+  theme_bw()
