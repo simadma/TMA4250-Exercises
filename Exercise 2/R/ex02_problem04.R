d5b95931bbe6e1db29b4d1603abc6ad7c4e282fa
karinalilleborge <karina.lilleborge@gmail.com>
2021-03-24 18:47:10 +0100

    tested the code in prob4

--- Exercise 2/R/ex02_problem04.R
+++ Exercise 2/R/ex02_problem04.R
@@ -69,7 +69,7 @@ iterative_sim_Strauss <- function(k, tau_0, phi_0, phi_1, n_iter = 1000) {
 }
 
 
-test <- iterative_sim_Strauss(nrow(cells), tau_0 = 0.08, phi_0 = 5, phi_1 = 6)
+test <- iterative_sim_Strauss(nrow(cells), tau_0 = 0.02, phi_0 = 7000, phi_1 = 9)
 
 test %>% 
   ggplot(aes(x, y)) +
@@ -79,3 +79,42 @@ test %>%
     ylim = c(0, 1)
   ) +
   theme_bw()
+
+#Monte carlo test on the parameters with the L-function
+sim_multiple_Strauss <- function(k, tau_0, phi_0, phi_1, nsim=100) {
+  result <- matrix(0, nrow = nsim, ncol = 100) #hvorfor 100 kolonner?
+  for (i in 1:nsim){
+    result[i, ] <- L(iterative_sim_Strauss(k, tau_0, phi_0, phi_1))$y
+  }
+  result
+}
+
+#Monte carlo test on the parameters
+#disse er ikke rett:/ er vanskelig å få den sånn flat på starten...
+tau_0 <- 0.02
+phi_0 <- 7000
+phi_1 <- 9
+set.seed(99)
+L_sim <- sim_multiple_Strauss(nrow(cells), tau_0 = tau_0, phi_0 = phi_0, phi_1 = phi_1, nsim=10) #100 tar kjeeempelang tid
+level <- 0.1
+predint <- pred_interval(L_sim, level)
+lab <- sprintf("%.2f-interval", 1 - level)
+tib <- with(predint, cbind(L(cells), lower = lower, upper = upper))
+p1 <- ggplot(tib, aes(x, y)) +
+  geom_ribbon(aes(ymin = lower, ymax = upper, fill = lab), alpha = 0.2) +
+  geom_line(aes(color = "Empirical L-function")) +
+  coord_fixed(
+    xlim=c(0, .5), ylim=c(0, .5)
+  ) +
+  labs(x = "t", y = "L(t)", color = "", fill = "") +
+  scale_color_manual(values = "blue") +
+  scale_fill_manual(values = "black") +
+  guides(fill  = guide_legend(order = 2),
+         color = guide_legend(order = 1)) +
+  theme_bw() +
+  theme(
+    legend.margin = margin(),
+    legend.spacing.y = unit(0, "mm"),
+    legend.background = element_blank()
+  )
+p1
