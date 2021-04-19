217c16577eb0e1bb3bcd944b77e68590515d385d
karinalilleborge <karina.lilleborge@gmail.com>
2021-04-19 14:18:17 +0200

    added code file for problem 1

---
+++ Exercise 3/R/ex03_problem01.R
@@ -0,0 +1,11 @@
+#Project 3 in TMA4250
+#Authors: Mads Adrian Simonsen and Karina Lilleborge
+#PROBLEM 1
+
+
+#libraries
+library(spatial)
+
+#paths
+data_path <- "./Exercise 2/Data/"
+fig_path <- "./Exercise 2/Figures/"
